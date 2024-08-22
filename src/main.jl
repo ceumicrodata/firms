668dda2420fe0f4e7913dc5e5667d89692183c1e
andrasvereckei <andras.vereckei@gmail.com>
2024-08-22 15:53:03 +0200

    Update figure 4-6

--- src/main.jl
+++ src/main.jl
@@ -8,6 +8,7 @@ include("functions.jl")
 @target balance_data = create_balance_data()
 @target balance = clean_balance(balance_data)
 @target agg = aggregate(balance)
+@target aggregate_survival = aggregate_survival(balance)
 
 function main()
     @get agg
@@ -15,9 +16,11 @@ function main()
         @generate category = size_category
         @replace category = "foreign" @if ownership == "foreign"
         @replace category = "state" @if ownership == "state"
-        @collapse gdp = sum(gdp) emp = sum(emp) sales = sum(sales) Export = sum(Export) n_firms = sum(n_firms), by(category, year)
+        @collapse mean_growth = sum(mean_growth) firm_start = sum(firm_start) gdp = sum(gdp) emp = sum(emp) sales = sum(sales) Export = sum(Export) n_firms = sum(n_firms) n_firms_export = sum(n_firms_export) hlemp = sum(hlemp) n_firms_age = sum(n_firms_age), by(category, year)
         @generate gdp_per_worker = gdp / emp 
         @generate export_share = Export / sales
+        @egen max_n = max(cond(n_firms_age == 0, n_firms, 0))
+        @generate survival = 100 * n_firms / max_n
         @sort category year
     end
     bysize = @with agg begin
@@ -27,8 +30,15 @@ function main()
         @drop @if ownership == "state"
         @collapse gdp = sum(gdp) emp = sum(emp) sales = sum(sales) Export = sum(Export), by(category, year)
         @generate gdp_per_worker = gdp / emp 
+        @generate sales_per_worker = sales / emp 
         @generate export_share = Export / sales
         @sort category year
+    end
+    @get aggregate_survival
+    allsurv = @with aggregate_survival begin
+        @generate category = size_category
+        @egen max_n = max(cond(firmage2 == 1, n_firms, 0)), by(category)
+        @generate survival = 100 * n_firms / max_n
     end
 
     set_aog_theme!()
@@ -40,9 +50,14 @@ function main()
         save("$(figure_folder)/$(y).png", fig, px_per_unit = 1)
     end
 
-    ts_plot(all, :n_firms)
-    ts_plot(bysize, :gdp_per_worker)
-    ts_plot(bysize, :export_share)
+    fig1 = ts_plot(all, :n_firms)
+    fig2 = ts_plot(bysize, :sales_per_worker)
+    fig3 = ts_plot(bysize, :gdp_per_worker)
+    fig4 = ts_plot(bysize, :export_share)
+    fig5 = ts_plot(all, :n_firms_export)
+    fig6 = ts_plot(all, :firm_start)
+    fig7 = ts_plot(allsurv, :survival)
+    fig8 = ts_plot(all, :mean_growth)
 end
 
 main()
